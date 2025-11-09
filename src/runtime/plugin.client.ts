cb29a3232312c4e1b6b015456ceb6902bf618b46
LePepe <gonzalezmunozjosearmando@gmail.com>
2025-11-08 23:02:01 -0300

    chore(package.json): update dependencies and devDependencies to latest versions for improved stability and features fix(plugin.client.ts): change process.client to import.meta.client for compatibility with the latest Nuxt version fix(tsconfig.json): update tsconfig path to reflect new directory structure for correct TypeScript configuration

--- src/runtime/plugin.client.ts
+++ src/runtime/plugin.client.ts
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(async (_nuxtApp) => {
-  if (process.client) {
+  if (import.meta.client) {
     const { defineCustomElements } = await import('@le-pepe/snow-effect/loader')
     _nuxtApp.hook('app:beforeMount', () => {
       defineCustomElements(window)
